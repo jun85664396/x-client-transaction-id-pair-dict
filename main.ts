eee3d156985c9da2766a7423e0fcaaa6bee2c592
Phil <jun85664396@gmail.com>
2026-03-04 20:03:44 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ interface Dict {
 }
 
 const dict: Dict[] = [];
-const max = 30;
+const max = 50;
 const browser = await puppeteer.launch({
   headless: true,
   args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage", "--disable-accelerated-2d-canvas", "--disable-gpu"],
